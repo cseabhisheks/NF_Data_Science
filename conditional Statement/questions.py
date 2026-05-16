99b9bd102b20b0a5683e5fa95af252666eb47cef
cseabhisheks <singhabhishek.engineer@gmail.com>
2026-05-16 13:52:45 +0530

    left question from condition statement are solved

--- conditional Statement/questions.py
+++ conditional Statement/questions.py
@@ -68,8 +68,27 @@ elif operator=='/':
       print("div: ",operand1/opearnd2)
 
 # 7. Write a program to calculate income tax according to salary ranges. 
-
+salary=5667888999
+if salary <= 250000:
+    tax = 0
+elif salary <= 500000:
+    tax = salary * 0.05
+elif salary <= 1000000:
+    tax = salary * 0.10
+else:
+    tax = salary * 0.15
+print("Income Tax =", tax)
 # 8. Write a program to check login authentication using username and password conditions. 
+db={
+    'user':'amit',
+    'password':'1234'
+}
+user='amit'
+password="1234"
+if user==db['user'] and password==db['password']:
+    print("you are authenticated")
+else:
+    print("user not exist")
 # 9. Write a program to determine whether a point lies in First quadrant, Second quadrant, Third 
 # quadrant, Fourth quadrant, On axis, or At origin. 
 x=-5
@@ -87,3 +106,14 @@ elif x<0 and y<0:
 else:
     print("fourth quadrant")
 # 10. Write a program to assign grades based on marks and display distinction for high scores. 
+marks = 80
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Fail")
